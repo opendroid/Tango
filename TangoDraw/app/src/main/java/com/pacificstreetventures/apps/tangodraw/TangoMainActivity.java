76785ce7cc74006b9c77ad29897512be0f402770
opendriod <ajaythakur@hotmail.com>
2015-04-23 17:40:13 -0700

    Getting Tango Started
    Copies basic QuickStart.java logic into this app.

--- TangoDraw/app/src/main/java/com/pacificstreetventures/apps/tangodraw/TangoMainActivity.java
+++ TangoDraw/app/src/main/java/com/pacificstreetventures/apps/tangodraw/TangoMainActivity.java
@@ -1,17 +1,157 @@
 package com.pacificstreetventures.apps.tangodraw;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+
+import java.util.ArrayList;
 
 
 public class TangoMainActivity extends ActionBarActivity {
+    private static final String TAG = TangoMainActivity.class.getSimpleName();
+    private static final String sTranslationFormat = "Translation: %f, %f, %f";
+    private static final String sRotationFormat = "Rotation: %f, %f, %f, %f";
+
+    private TextView mTranslationTextView;
+    private TextView mRotationTextView;
+
+    private Tango mTango;
+    private TangoConfig mConfig;
+    private boolean mIsTangoServiceConnected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tango_main);
+
+        // Instantiate the Tango service
+        mTango = new Tango(this);
+
+        mTranslationTextView = (TextView) findViewById(R.id.translation_text_view);
+        mRotationTextView = (TextView) findViewById(R.id.rotation_text_view);
+
+        // Set up Tango configuration for motion tracking
+        // If you want to use other APIs, add more appropriate to the config
+        // like: mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true)
+        mConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_CURRENT);
+        mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!mIsTangoServiceConnected) {
+            startActivityForResult(
+                    Tango.getRequestPermissionIntent(Tango.PERMISSIONTYPE_MOTION_TRACKING),
+                    Tango.TANGO_INTENT_ACTIVITYCODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == Tango.TANGO_INTENT_ACTIVITYCODE) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this,
+                        "This app requires Motion Tracking permission!",
+                        Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+            try {
+                setTangoListeners();
+            } catch (TangoErrorException e) {
+                Toast.makeText(this, "Tango Error! Restart the app!",
+                        Toast.LENGTH_SHORT).show();
+            }
+            try {
+                mTango.connect(mConfig);
+                mIsTangoServiceConnected = true;
+            } catch (TangoOutOfDateException e) {
+                Toast.makeText(getApplicationContext(),
+                        "Tango Service out of date!", Toast.LENGTH_SHORT)
+                        .show();
+            } catch (TangoErrorException e) {
+                Toast.makeText(getApplicationContext(),
+                        "Tango Error! Restart the app!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    private void setTangoListeners() {
+        // Select coordinate frame pairs
+        ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+
+        // Add a listener for Tango pose data
+        mTango.connectListener(framePairs, new Tango.OnTangoUpdateListener() {
+
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onPoseAvailable(TangoPoseData pose) {
+                // Format Translation and Rotation data
+                final String translationMsg = String.format(sTranslationFormat,
+                        pose.translation[0], pose.translation[1],
+                        pose.translation[2]);
+                final String rotationMsg = String.format(sRotationFormat,
+                        pose.rotation[0], pose.rotation[1], pose.rotation[2],
+                        pose.rotation[3]);
+
+                // Output to LogCat
+                String logMsg = translationMsg + " | " + rotationMsg;
+                Log.i(TAG, logMsg);
+
+                // Display data in TextViews. This must be done inside a
+                // runOnUiThread call because
+                // it affects the UI, which will cause an error if performed
+                // from the Tango
+                // service thread
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTranslationTextView.setText(translationMsg);
+                        mRotationTextView.setText(rotationMsg);
+                    }
+                });
+            }
+
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData arg0) {
+                // Ignoring XyzIj data
+            }
+
+            @Override
+            public void onTangoEvent(TangoEvent arg0) {
+                // Ignoring TangoEvents
+            }
+
+            @Override
+            public void onFrameAvailable(int arg0) {
+                // Ignoring onFrameAvailable Events
+
+            }
+
+        });
     }
 
     @Override
